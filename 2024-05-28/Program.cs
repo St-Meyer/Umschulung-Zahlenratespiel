f27c5ef0517067d472a83f9744c6ed7aa867db09
Meyer <Meyer.92@gmx.de>
2024-05-28 13:01:15 +0200

    Projektdateien hinzufügen.

---
+++ 2024-05-28/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2024_05_28
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Settings
+            Console.OutputEncoding = Encoding.Unicode;
+
+            // Declaration
+            int hiddenNumber, input, counter, maxGuessCount;
+            bool guessedCorrectly;
+
+            // Initialisation
+            hiddenNumber = new Random().Next(1, 99);
+            guessedCorrectly = false;
+            counter = 1;
+            maxGuessCount = 10;
+
+            // Opening Message
+            Console.WriteLine("Willkommen beim Zahlen-Ratespiel! ");
+            Console.WriteLine("Sie müssen eine Zahl erraten und es wird gesagt, " + 
+                              "ob Sie höher oder tiefer tippen müssen");
+            Console.WriteLine("Sie haben {0} Versuche!", maxGuessCount);
+
+            // EVA
+            while (guessedCorrectly == false && counter <= maxGuessCount)
+            {        
+                Console.Write("Dies ist Ihr {0}. Versuch. " +
+                              "Bitte geben Sie Ihren Tipp: ", counter++);
+                input = int.Parse(Console.ReadLine());
+
+                if (input < hiddenNumber)
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("Deine Zahl ist zu niedrig!");
+                }
+                else if (input > hiddenNumber)
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("Deine Zahl ist zu hoch!");
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.WriteLine("Herzlichen Glückwunsch! Deine Zahl ist korrekt! 😁🎉");
+                    guessedCorrectly = true;
+                }
+                Console.ResetColor();
+                if (counter > maxGuessCount)
+                    Console.WriteLine("Die Zahl war {0}.", hiddenNumber);          
+            }
+        }
+    }
+}
---
+++ 2024-05-28/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über die folgenden
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die einer Assembly zugeordnet sind.
+[assembly: AssemblyTitle("2024-05-28")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("bfw Unternehmen für Bildung")]
+[assembly: AssemblyProduct("2024-05-28")]
+[assembly: AssemblyCopyright("Copyright © bfw Unternehmen für Bildung 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Durch Festlegen von ComVisible auf FALSE werden die Typen in dieser Assembly
+// für COM-Komponenten unsichtbar.  Wenn Sie auf einen Typ in dieser Assembly von
+// COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("ad3347fb-16fb-4eb5-85bd-b84545a04ba9")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion
+//      Buildnummer
+//      Revision
+//
+// Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
+// indem Sie "*" wie unten gezeigt eingeben:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
